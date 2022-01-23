f80df08f6a575bc8f79c435a830da622954d12a4
pcragnol <94771135+pcragnol@users.noreply.github.com>
2022-01-22 22:39:04 -0500

    Color-coded time blocks and engineered save buttons.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,35 @@
-setInterval(function() {
-  $('#currentDay').text(moment().format('LLLL'));
+$("#currentDay").text(moment().format("LLLL"));
+
+setInterval(function () {
+  $("#currentDay").text(moment().format("LLLL"));
 }, 1000);
 
-$('textarea').each(function() {
-  $(this).addClass('future');
+$("textarea").each(function () {
+  if ($(this).data("hour") < moment().format("HH")) {
+    $(this).addClass("past");
+  } else if ($(this).data("hour") > moment().format("HH")) {
+    $(this).addClass("future");
+  } else {
+    $(this).addClass("present");
+  }
 });
+
+console.log($("#test").parent().parent().data("hour"));
+
+$(".saveBtn").on("click", function (event) {
+  console.log($(event.target).parent("tr").children("th").text());
+  console.log($(event.target).parent("tr").children("td").eq(0).children().text());
+  localStorage.setItem(`${$(event.target).parent("tr").children("th").text()} event`, $(event.target).parent("tr").children("td").eq(0).children().val());
+});
+
+// $("button").each(function () {
+//   $(this).addEventListener("click", function () {
+//     console.log("test");
+//   });
+// });
+
+// $("button").each($(this).addEventListener("click", function () {
+//   console.log("test");
+// }));
+
+// $("button").each(addEventListener("click", console.log("test")));
