4a6e6ec96839ecf1cdf4bee1c5efe426b2cd1bdc
pcragnol <94771135+pcragnol@users.noreply.github.com>
2022-01-25 21:56:10 -0500

    Refined and commented script.js and polished web page appearance

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Work_Day_Scheduler
-Work Day Scheduler
+
+## Description
+
+For this project, I created a work day calendar. Talk about Moment.js, DOM Traversal, Datasets, local storage.
+
+### Links
+
+[Work Day Scheduler](https://pcragnol.github.io/Work_Day_Scheduler/)
+
+[Screenshot]
+
+#### Credits
+
+Original code provided by Trilogy Educational Services
+
+---
+© 2022 Peter Cragnoline
+
+Licensed under [MIT License](LICENSE)
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,37 @@
-$("#currentDay").text(moment().format("LLLL"));
-
-setInterval(function () {
+// Displays current time to page
+function displayTime() {
   $("#currentDay").text(moment().format("LLLL"));
+}
+
+// Applies color to each textarea depending on relation to current time
+function setColor() {
+  $("textarea").each(function () {
+    if ($(this).data("hour") < moment().format("HH")) {
+      $(this).addClass("past");
+    } else if ($(this).data("hour") > moment().format("HH")) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+}
+
+// Calls displayTime and setColor functions when page is loaded
+displayTime();
+setColor();
+
+// Continues to call displayTime and setColor functions every second
+setInterval(function () {
+  displayTime();
+  setColor();
 }, 1000);
 
-$("textarea").each(function () {
-  if ($(this).data("hour") < moment().format("HH")) {
-    $(this).addClass("past");
-  } else if ($(this).data("hour") > moment().format("HH")) {
-    $(this).addClass("future");
-  } else {
-    $(this).addClass("present");
-  }
-});
-
+// Saves textarea content to local storage when corresponding save button is clicked
 $(".saveBtn").on("click", function (event) {
-  // localStorage.setItem(`${$(event.target).parent("tr").children("th").text()} event`, $(event.target).parent("tr").children("td").eq(0).children().val());
-  localStorage.setItem(`${$(event.target).parent("tr").children("td").eq(0).children().data("hour")} event`, $(event.target).parent("tr").children("td").eq(0).children().val());
+  localStorage.setItem(`${$(event.target).data("hour")} event`, $(event.target).prev().children().val());
 });
 
+// Populates each textarea with correspoinding local storage data when page is loaded
 $("textarea").each(function () {
-  // $(this).val(localStorage.getItem(`${$(this).parent("tr").children("th").text()} event`));
   $(this).val(localStorage.getItem(`${$(this).data("hour")} event`));
 });
